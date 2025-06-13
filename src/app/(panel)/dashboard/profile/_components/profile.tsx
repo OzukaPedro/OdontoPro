1ee96bfe3ee2b2c4df84156046be08d4cc8f44a5
Pedro Campagnoli <pedrocampagnoli28@gmail.com>
2025-06-13 11:19:39 -0300

    remodelagem

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,16 +1,16 @@
 "use client";
-
-import { useProfileForm } from "./profileForm";
+import { useState } from "react";
+import { ProfileFormData, useProfileForm } from "./profile-form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-  FormDescription,
 } from "@/components/ui/form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -19,32 +19,84 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
-import imgTest from "../../../../../../public/foto1.png";
-import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-import { TimeZonesFilter } from "../_actions/timeZonesFilter";
-import { ProfileFieldForm } from "./profileField";
-import { ProfileTimeDialog } from "./profileTimeDialog";
-import { TimeZoneField } from "./timeZoneField";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+import imgTest from "../../../../../../public/foto1.png";
+import { cn } from "@/lib/utils";
 
 export function ProfileContent() {
+  const [selectedHours, setSelectedHours] = useState<string[]>([]);
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
   const form = useProfileForm();
 
-  const timeZones = TimeZonesFilter();
+  function generateTimeSlots(): string[] {
+    const hours: string[] = [];
+
+    for (let i = 8; i <= 24; i++) {
+      for (let j = 0; j < 2; j++) {
+        const hour = i.toString().padStart(2, "0");
+        const minute = (j * 30).toString().padStart(2, "0");
+        hours.push(`${hour}:${minute}`);
+      }
+    }
+
+    return hours;
+  }
+
+  const hours = generateTimeSlots();
+
+  function toggleHour(hour: string) {
+    setSelectedHours((prev) =>
+      prev.includes(hour)
+        ? prev.filter((h) => h !== hour)
+        : [...prev, hour].sort()
+    );
+  }
+
+  const timeZones = Intl.supportedValuesOf("timeZone").filter(
+    (zone) =>
+      zone.startsWith("America/Sao_Paulo") ||
+      zone.startsWith("America/Fortaleza") ||
+      zone.startsWith("America/Recife") ||
+      zone.startsWith("America/Bahia") ||
+      zone.startsWith("America/Belem") ||
+      zone.startsWith("America/Manaus") ||
+      zone.startsWith("America/Cuiaba") ||
+      zone.startsWith("America/Boa_Vista")
+  );
+
+  async function onSubmit(values: ProfileFormData) {
+    const profileData = {
+      ...values,
+      times: selectedHours,
+    };
+    console.log("values: ", profileData);
+  }
 
   return (
     <div className="mx-auto">
       <Form {...form}>
-        <form>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <Card>
             <CardHeader>
               <CardTitle>Meu Perfil</CardTitle>
             </CardHeader>
             <CardContent className="space-y-6">
-              <div className="flex justify-center ">
-                <div className="relative w-40 h-40 rounded-full overflow-hidden bg-gray-200">
+              <div className="flex justify-center">
+                <div className="bg-gray-200 relative h-40 w-40 rounded-full overflow-hidden">
                   <Image
                     src={imgTest}
                     alt="Foto da clinica"
@@ -54,44 +106,183 @@ export function ProfileContent() {
                 </div>
               </div>
 
-              <ProfileFieldForm
-                name="name"
-                label="Nome"
-                placeholder="Digite o nome da clinica"
-              />
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Nome completo
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o nome da clinica..."
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <ProfileFieldForm
-                name="address"
-                label="Endereço"
-                placeholder="Digite o endereço da clinica"
-              />
+                <FormField
+                  control={form.control}
+                  name="address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Endereço completo:
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Digite o endereço da clinica..."
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <ProfileFieldForm
-                name="phone"
-                label="Telefone"
-                placeholder="Digite o telefone "
-              />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Telefone</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Digite o telefone..." />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-              <ProfileFieldForm
-                type="select"
-                name="status"
-                label="Status"
-                items={[
-                  { value: "active", text: "Ativo" },
-                  { value: "inactive", text: "Inativo" },
-                ]}
-              />
+                <FormField
+                  control={form.control}
+                  name="status"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Status da clinica
+                      </FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value ? "active" : "inactive"}
+                        >
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Selecione o status da clincia" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="active">
+                              ATIVO (clinica aberta)
+                            </SelectItem>
+                            <SelectItem value="inactive">
+                              INATIVO (clinica fechada)
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
 
-              <ProfileTimeDialog />
+                <div className="space-y-2">
+                  <Label className="font-semibold">
+                    Configurar horários da clinica
+                  </Label>
 
-              <TimeZoneField />
+                  <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant="outline"
+                        className="w-full justify-between"
+                      >
+                        Clique aqui para selecionar horários
+                        <ArrowRight className="w-5 h-5" />
+                      </Button>
+                    </DialogTrigger>
 
-              <Button
-                className="font-semibold w-full bg-emerald-500 hover:bg-emerald-400"
-                type="submit"
-              >
-                Salvar Alterações
-              </Button>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Horários da clinica</DialogTitle>
+                        <DialogDescription>
+                          Selecione abaixo os horários de funcionamento da
+                          clinica:
+                        </DialogDescription>
+                      </DialogHeader>
+
+                      <section className="py-4">
+                        <p className="text-sm text-muted-foreground mb-2">
+                          Clique nos horários abaixo para marcar ou desmcar:
+                        </p>
+
+                        <div className="grid grid-cols-5 gap-2">
+                          {hours.map((hour) => (
+                            <Button
+                              key={hour}
+                              variant="outline"
+                              className={cn(
+                                "h-10",
+                                selectedHours.includes(hour) &&
+                                  "border-2 border-emerald-500 text-primary"
+                              )}
+                              onClick={() => toggleHour(hour)}
+                            >
+                              {hour}
+                            </Button>
+                          ))}
+                        </div>
+                      </section>
+
+                      <Button
+                        className="w-full"
+                        onClick={() => setDialogIsOpen(false)}
+                      >
+                        Fechar modal
+                      </Button>
+                    </DialogContent>
+                  </Dialog>
+                </div>
+
+                <FormField
+                  control={form.control}
+                  name="timeZone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">
+                        Selecione o fuso horário
+                      </FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Selecione o seu fuso horário" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {timeZones.map((zone) => (
+                              <SelectItem key={zone} value={zone}>
+                                {zone}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full bg-emerald-500 hover:bg-emerald-400"
+                >
+                  Salvar alterações
+                </Button>
+              </div>
             </CardContent>
           </Card>
         </form>
